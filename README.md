6784f5e01971c110337ddbb03b58e0bff1562c04
Sofia Vazquez <sofia.vazquez.duarte@tecnico.ulisboa.pt>
2026-02-14 16:22:56 +0000

    We can get requests. Yay!

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # web_server_made_rusty
-Project from the Rust Programing language book
+A multi-threaded TCP/HTTP server built in Rust, following the implementation patterns from The Rust Programming Language (Chapter 20).
+
+## Motivation
+I am an Aerospace Engineer, and Embedded Software developer.  
+While I am comfortable with real-time constraints, memory-mapped I/O, and hardware protocols like SPI/I2C, I knew nothing about Computer next works.
+
+This project serves as way to learn a little about computer networks, while also practing my rust Programming skills :).
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+//! user imports
+use std::{
+    io::{prelude::*, BufReader}, // the std lib offers a module to listen to a TCP connection
+    net::{TcpListener, TcpStream},
+};
+
+const LCL_ADDR: &str = "127.0.0.1:7878";
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind(LCL_ADDR).unwrap(); // bind kind works like new
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap(); //unwrap because we know that the connection will
+
+        //be established for certain ( we cold also use ?)
+        println!("Connection established"); //if i load the browser after start runing the program,
+                                            //this will be printed
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&stream);
+    let http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+    println!("Request: {http_request:#?}");
 }
